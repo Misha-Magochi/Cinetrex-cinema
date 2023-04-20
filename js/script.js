d16139f28400bc86dc0cd63f0876351f0a557fc3
Misha Magochi <magochimisha@gmail.com>
2023-04-20 13:25:02 +0300

    animation

--- js/script.js
+++ js/script.js
@@ -1,45 +1,74 @@
 window.addEventListener('DOMContentLoaded', () => {
-let modalWin = document.getElementById('modal-win'),
-    modalLink = document.getElementById('link-id');
-
-
-    modalLink.onclick = function () {
-        modalWin.style.display = 'block';
-
-    };
-
-    window.onclick = function (e) {
-        if(e.target == modalWin) {
-            modalWin.style.display = 'none';
-        }
-        if(e.target == modalSign) {
-            modalSign.style.display = 'none';
-        }
-    } 
+    const btn1 = document.getElementById('btn1');
+    const btn2 = document.getElementById('btn2');
+    const btn3 = document.getElementById('btn3');
+    
+    const modal1 = document.getElementById('modal1');
+    const modal2 = document.getElementById('modal2');
+    const modal3 = document.getElementById('modal3');
+    
+    function openModal(modal) {
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+    }
+    
+    function closeModal(modal) {
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+    }
+    
+    btn1.addEventListener('click', () => {
+      openModal(modal1);
+      closeModal(modal2);
+      closeModal(modal3);
+    });
+    
+    btn2.addEventListener('click', () => {
+      openModal(modal2);
+      closeModal(modal1);
+      closeModal(modal3);
+    });
+    
+    btn3.addEventListener('click', () => {
+      openModal(modal3);
+      closeModal(modal1);
+      closeModal(modal2);
+    });
+    
+    window.addEventListener('click', (e) => {
+      if (e.target === modal1) {
+        closeModal(modal1);
+      }
+      if (e.target === modal2) {
+        closeModal(modal2);
+      }
+      if (e.target === modal3) {
+        closeModal(modal3);
+      }
+    });
 
 const modalSign = document.querySelector('.modal_sign'),
       modalSignBtn = document.querySelector('.btn_sing-in'),
       modalBtnSign = document.querySelector('.btn_log-in');
 
       modalSignBtn.onclick = function () {
-        modalSign.style.display = 'block';
+        modalSign.classList.add('show');
+        modalSign.classList.remove('hide');
         document.body.style.overflow = 'hidden';
       };
 
 
     modalBtnSign.onclick = function () {
-        modalSign.style.display = 'none';
+      modalSign.classList.remove('show');
+      modalSign.classList.add('hide');
         document.body.style.overflow = '';
-    }
+    };
 
     BtnSignIn = document.querySelector('.btn_container');
 
-    
-
     BtnSignIn.onclick = function () {
         BtnSignIn.style.display = 'none';
-                document.body.style.overflow = '';
-    }
+    };
 
     function showModalByScroll() {
         if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight -1) {
@@ -49,4 +78,38 @@ const modalSign = document.querySelector('.modal_sign'),
         }
         window.addEventListener('scroll', showModalByScroll);
 
+        const recX = document.querySelectorAll('.rec_trend-X');
+ 
+    let i;
+    for(i = 0; i < recX.length; i++) {
+        recX[i].onclick = function () {
+            let span = this.parentElement;
+            setTimeout(function() {
+                span.style.display = 'none';
+            }, 600)
+        }
+    }
+    /* ---------------------modal-Sign-up------------------- */
+    const signInBtn = document.getElementById('signInBtn');
+    const signUpBtn = document.getElementById('signUpBtn');
+    const signInModal = document.getElementById('signInModal');
+    const signUpModal = document.getElementById('signUpModal');
+    
+    signInBtn.addEventListener('click', function() {
+        signUpBtn.classList.remove('active');
+        signInBtn.classList.add('active');
+      signInModal.style.display = 'block';
+      signUpModal.style.display = 'none';
+
+    });
+    
+
+    signUpBtn.addEventListener('click', function() {
+        signInBtn.classList.remove('active');
+        signUpBtn.classList.add('active');
+      signUpModal.style.display = 'block';
+      signInModal.style.display = 'none';
+
+    });
+    
 })
